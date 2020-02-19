753a9e2f16914db0bf8d2c03944faa8227c9406d
ozbannot <ozbannot@gmail.com>
2020-02-19 20:49:58 +0900

    変数宣言

--- components/Message.vue
+++ components/Message.vue
@@ -4,8 +4,9 @@
      <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
    </div>
    <div class="message-container">
-     <div class="user-name">cohki0305</div>
-     <div class="message">今日も良い天気ですね</div>
+       <img v-bind:src="message.user.thumbnail" />
+      <div class="user-name">{{ displayName }}</div>
+     <div class="message">{{ message.text }}</div>
    </div>
  </div>
 </template>
@@ -30,3 +31,24 @@
  font-weight: bold;
 }
 </style>
+
+<script>
+export default {
+ data() {
+   return {
+     message: {
+       text: '今日も良い天気ですね',
+       user: {
+         thumbnail: 'https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg',
+         name: 'note'
+       }
+     }
+   }
+ },
+ computed: {
+   displayName() {
+     return `@${this.message.user.name}`
+   }
+ }
+}
+</script>
